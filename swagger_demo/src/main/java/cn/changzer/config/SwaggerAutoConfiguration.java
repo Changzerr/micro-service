d9301a7497eb7f97db287007e91ae5188e857e7c
lingqu <2784247628@qq.com>
2022-09-29 19:26:31 +0800

    swagger

--- swagger_demo/src/main/java/cn/changzer/config/SwaggerAutoConfiguration.java
+++ swagger_demo/src/main/java/cn/changzer/config/SwaggerAutoConfiguration.java
@@ -16,24 +16,24 @@ public class SwaggerAutoConfiguration {
     @Bean
     public Docket createRestApi1() {
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()).groupName("用户接口组")
+                .apiInfo(apiInfo())//.groupName("用户接口组")
                 .select()
                 //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("cn.changzer.controller.user"))
+                .apis(RequestHandlerSelectors.basePackage("cn.changzer.controller"))
                 .build();
         return docket;
     }
 
-    @Bean
-    public Docket createRestApi2() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()).groupName("菜单接口组")
-                .select()
-                //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("cn.changzer.controller.menu"))
-                .build();
-        return docket;
-    }
+    //@Bean
+    //public Docket createRestApi2() {
+    //    Docket docket = new Docket(DocumentationType.SWAGGER_2)
+    //            .apiInfo(apiInfo()).groupName("菜单接口组")
+    //            .select()
+    //            //为当前包路径
+    //            .apis(RequestHandlerSelectors.basePackage("cn.changzer.controller.menu"))
+    //            .build();
+    //    return docket;
+    //}
 
     //构建 api文档的详细信息
     private ApiInfo apiInfo() {
