142701b4a21110ecb36e143db4ad871c2d5cf075
lingqu <2784247628@qq.com>
2022-09-29 15:51:31 +0800

    swagger

--- swagger_demo/src/main/java/cn/changzer/SwaggerDemoApplication.java
+++ swagger_demo/src/main/java/cn/changzer/SwaggerDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwaggerDemoApplication {
+	//http://localhost:9000/swagger-ui.html
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerDemoApplication.class, args);
 	}
