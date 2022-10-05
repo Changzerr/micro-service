36bc169f781516d3668a9bd6fa7c1024c0e82dfa
lingqu <2784247628@qq.com>
2022-10-05 15:01:22 +0800

    log测试3

--- logback/logback_demo/src/main/java/cn/changzer/logback/LogBackTest.java
+++ logback/logback_demo/src/main/java/cn/changzer/logback/LogBackTest.java
@@ -1,6 +1,9 @@
 package cn.changzer.logback;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.junit.Test;
+import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,5 +23,35 @@ public class LogBackTest {
         logger.info("info ...");
         logger.warn("warn ...");
         logger.error("error ...");
+    }
+
+    @Test
+    public void test2(){
+        Logger logger = LoggerFactory.getLogger("cn.changzer.logback.HelloWorld");
+        //默认debug级别，从root logger 继承而来
+        logger.trace("trace ...");
+        logger.debug("debug ...");
+        logger.info("info ...");
+        logger.warn("warn ...");
+        logger.error("error ...");
+
+        LoggerContext iLoggerFactory = (LoggerContext)LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(iLoggerFactory);
+    }
+
+    /*
+     * 日志输出级别：ERROR > WARN > INFO > DEBUG > TRACE
+     * */
+
+    //测试默认的日志输出级别
+    @Test
+    public void test3(){
+        Logger logger = LoggerFactory.getLogger("cn.itcast.logback.HelloWorld");
+        logger.error("error ...");
+        logger.warn("warn ...");
+        logger.info("info ...");
+        logger.debug("debug ...");
+        //因为默认的输出级别为debug，所以这一条日志不会输出
+        logger.trace("trace ...");
     }
 }
