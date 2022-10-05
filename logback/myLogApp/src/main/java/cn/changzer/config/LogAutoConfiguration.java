fdd88044d94bd58f9a596674448637de2918247f
lingqu <2784247628@qq.com>
2022-10-05 17:09:18 +0800

    myLogApp

---
+++ logback/myLogApp/src/main/java/cn/changzer/config/LogAutoConfiguration.java
@@ -0,0 +1,20 @@
+package cn.changzer.config;
+/*
+*日志配置类
+*/
+
+import cn.changzer.service.LogService;
+import com.itheima.pinda.log.event.SysLogListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogAutoConfiguration {
+    //自动配置日志监听器组件
+    @Bean
+    @ConditionalOnMissingBean
+    public SysLogListener sysLogListener(LogService logService){
+        return new SysLogListener(optLogDTO -> logService.saveLog(optLogDTO));
+    }
+}
