4279bcd7ebb32e41189086ed657df79aef823874
lingqu <2784247628@qq.com>
2022-10-05 20:50:57 +0800

    初始化参数解析器项目

---
+++ argumentResolver/argumentResolver_demo/src/main/java/cn/changzer/controller/UserController.java
@@ -0,0 +1,18 @@
+package cn.changzer.controller;
+
+import cn.changzer.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    //获取当前系统登录用户
+    @GetMapping("/getCurrentUser")
+    public String getCurrentUser(User user) {
+        String name = user.getUsername();
+        System.out.println("UserController getCurrentUser方法...");
+        return user.toString();
+    }
+}
---
+++ argumentResolver/argumentResolver_demo/src/main/java/cn/changzer/entity/User.java
@@ -0,0 +1,13 @@
+package cn.changzer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+    private Long id;
+    private String username;
+}
