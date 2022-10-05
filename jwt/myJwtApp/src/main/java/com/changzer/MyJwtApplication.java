821ee379c2094ce6b0210acd05fdc1def4087f03
lingqu <2784247628@qq.com>
2022-10-05 20:20:01 +0800

    工程myJwtApp

---
+++ jwt/myJwtApp/src/main/java/com/changzer/MyJwtApplication.java
@@ -0,0 +1,13 @@
+package com.changzer;
+
+import com.itheima.pinda.auth.server.EnableAuthServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAuthServer //启用jwt服务端认证功能
+public class MyJwtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyJwtApplication.class,args);
+    }
+}
